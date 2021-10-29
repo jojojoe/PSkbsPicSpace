e2c382c651ecaf35e7db834db1edb6e3dd51d213
轩张 <zx804463232@gmail.com>
2021-10-29 19:17:43 +0800

    add 2

--- PSkbsPicSpace/AppDelegate.swift
+++ PSkbsPicSpace/AppDelegate.swift
@@ -6,17 +6,32 @@
 //
 
 import UIKit
-
+import AppTrackingTransparency
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        NotificationCenter.default.addObserver(self, selector: #selector(applicDidBecomeActiveNotifi(_:)), name: UIApplication.didBecomeActiveNotification, object: nil)
+        registerNotifications(application)
+        
+          
         return true
     }
 
+    @objc func applicDidBecomeActiveNotifi(_ notifi: Notification) {
+        // Start the SDK (start the IDFA timeout set above, for iOS 14 or later)
+        trackeringAuthor()
+    }
+    func trackeringAuthor() {
+       
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization(completionHandler: {[weak self] status in
+                guard let `self` = self else {return}
+                
+            })
+        }
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -34,3 +49,55 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+
+
+extension AppDelegate {
+    // 注册远程推送通知
+    func registerNotifications(_ application: UIApplication) {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.getNotificationSettings { (setting) in
+            if setting.authorizationStatus == .notDetermined {
+                center.requestAuthorization(options: [.badge,.sound,.alert]) { (result, error) in
+                    if (result) {
+                        if !(error != nil) {
+                            // 注册成功
+                            DispatchQueue.main.async {
+                                application.registerForRemoteNotifications()
+                            }
+                        }
+                    } else {
+                        //用户不允许推送
+                    }
+                }
+            } else if (setting.authorizationStatus == .denied){
+                // 申请用户权限被拒
+            } else if (setting.authorizationStatus == .authorized){
+                // 用户已授权（再次获取dt）
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            } else {
+                // 未知错误
+            }
+        }
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let body = notification.request.content.body
+        notification.request.content.userInfo
+        print(body)
+    }
+
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("")
+        let categoryIdentifier = response.notification.request.content.categoryIdentifier
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?) {
+        
+    }
+}
---
+++ PSkbsPicSpace/MainVC/PSMainVC.swift
@@ -0,0 +1,15 @@
+//
+//  PSMainVC.swift
+//  PSkbsPicSpace
+//
+//  Created by JOJO on 2021/10/29.
+//
+
+import UIKit
+
+class PSMainVC: UIViewController {
+
+    override func viewDidLoad() {
+        
+    }
+}
